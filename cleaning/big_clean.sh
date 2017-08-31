896d07a67e294cd186ea0b4d58e75531f0b5bf4b
Sphat001 <sphat001@morris.umn.edu>
2017-08-31 18:58:51 -0500

    Created our last argument tar_file
    Moved the cleaned tar_file from the scratch directory into the clean files

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/bash
+#Charles Menne and Jacob Sphatt
 
+
+# First argument tgz file
 tar_file=$1
 
-SCRATCH=$2
+# Second argument scratch directory
+scratch=$2
 
-MainDr=$(pwd)
-tar -xf $tar_file -C $SCRATCH
+# Copies the tar_file which then stores it as the Main variable
+Main=$(echo $tar_file | cut -f1 -d  '.')
 
+# Start directory
+here=$(pwd)
 
-for j in $MainDr; do
-    grep -l "DELETE ME!" $j | xargs rm -f
-done
+# Extracts the contents of the tar_file into the scratch directory
+tar -xzf $tar_file -C $scratch
 
-cd ..
-tar -zcf cleaned_$tar_file
+# Changes into the scratch directory
+cd $scratch
+
+# Finds and Removes all the files containing "DELETE ME!"
+rm `grep -lir "DELETE ME!" $Main`
+
+# Creates the new clean tar_file
+tar -czf cleaned_$tar_file $Main
+
+# Moves the cleaned tar_archive from the scratch directory into the clean files
+mv cleaned_$tar_file $here
+
